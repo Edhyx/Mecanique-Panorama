63cecb7a5e1cb88fbc95ae29433f60354732def5
Maxime FELICI <maxilinks@gmail.com>
2018-11-16 14:05:34 +0100

    Interval interface finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+import Tkinter as tk
 from datetime import datetime, timedelta
 import subprocess
 
@@ -136,10 +137,90 @@ def info_button():
     label.grid(row=0, column=1)
 
 def intervall_button():
+
+    def intervall_ok():
+        init_interface()
+
     for widget in frame.winfo_children():
         widget.destroy()
     btn_back = Button(frame, text='Back', command=init_interface) #create a button inside frame
-    btn_back.grid(row=0, column=0, padx=25, pady=25, sticky=N+W)
+    btn_back.place(relx=0.025, rely=0.042, height=29, width=58)
+
+    Spinbox1 = tk.Spinbox(frame, from_=1.0, to=100.0)
+    Spinbox1.place(relx=0.238, rely=0.521, relheight=0.125
+                , relwidth=0.075)
+    Spinbox1.configure(activebackground="#f9f9f9")
+    Spinbox1.configure(background="white")
+    Spinbox1.configure(highlightbackground="black")
+    Spinbox1.configure(justify='center')
+    Spinbox1.configure(selectbackground="#c4c4c4")
+    Spinbox1.configure(from_=0, to=24)
+    Spinbox1.configure(width=48)
+
+    Spinbox1_1 = tk.Spinbox(frame, from_=1.0, to=100.0)
+    Spinbox1_1.place(relx=0.563, rely=0.521, relheight=0.125
+                , relwidth=0.075)
+    Spinbox1_1.configure(activebackground="#f9f9f9")
+    Spinbox1_1.configure(background="white")
+    Spinbox1_1.configure(highlightbackground="black")
+    Spinbox1_1.configure(justify='center')
+    Spinbox1_1.configure(selectbackground="#c4c4c4")
+    Spinbox1_1.configure(from_=0, to=60)
+
+    Spinbox1_2 = tk.Spinbox(frame, from_=1.0, to=100.0)
+    Spinbox1_2.place(relx=0.388, rely=0.521, relheight=0.125
+                , relwidth=0.075)
+    Spinbox1_2.configure(activebackground="#f9f9f9")
+    Spinbox1_2.configure(background="white")
+    Spinbox1_2.configure(highlightbackground="black")
+    Spinbox1_2.configure(justify='center')
+    Spinbox1_2.configure(selectbackground="#c4c4c4")
+    Spinbox1_2.configure(from_=0, to=60)
+
+    Label1 = tk.Label(frame)
+    Label1.place(relx=0.325, rely=0.563, height=21, width=42)
+    Label1.configure(text='''Hours''')
+
+    Label2 = tk.Label(frame)
+    Label2.place(relx=0.475, rely=0.563, height=21, width=56)
+    Label2.configure(text='''Minutes''')
+
+    Label3 = tk.Label(frame)
+    Label3.place(relx=0.65, rely=0.563, height=21, width=58)
+    Label3.configure(text='''Seconds''')
+
+    ok_button = tk.Button(frame)
+    ok_button.place(relx=0.913, rely=0.917, height=29, width=45)
+    ok_button.configure(activebackground="#d9d9d9")
+    ok_button.configure(text='''OK''')
+    ok_button.configure(command=intervall_ok)
+
+    Label4 = tk.Label(frame)
+    Label4.place(relx=0.063, rely=0.542, height=41, width=128)
+    Label4.configure(font='Helvetica 20 bold')
+    Label4.configure(text='''Duration :''')
+    Label4.configure(width=128)
+
+    Label5 = tk.Label(frame)
+    Label5.place(relx=0.063, rely=0.292, height=41, width=128)
+    Label5.configure(activebackground="#f9f9f9")
+    Label5.configure(font='Helvetica 20 bold')
+    Label5.configure(justify='left')
+    Label5.configure(text='''Interval :''')
+
+    Spinbox_interval = tk.Spinbox(frame, from_=1.0, to=100.0)
+    Spinbox_interval.place(relx=0.238, rely=0.271, relheight=0.125
+            , relwidth=0.075)
+    Spinbox_interval.configure(activebackground="#f9f9f9")
+    Spinbox_interval.configure(background="white")
+    Spinbox_interval.configure(highlightbackground="black")
+    Spinbox_interval.configure(justify='center')
+    Spinbox_interval.configure(from_=0, to=300)
+
+    Label6 = tk.Label(frame)
+    Label6.place(relx=0.338, rely=0.313, height=21, width=58)
+    Label6.configure(text='''Seconds''')
+
 
 
 
