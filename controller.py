3c4b5c520cd746040cbd49bf65f486e533d26da6
Maxime FELICI <maxilinks@gmail.com>
2018-11-24 16:52:37 +0100

    Add controller functions

--- controller.py
+++ controller.py
@@ -17,3 +17,18 @@ class Controller:
         p = subprocess.Popen("ls", stdout=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
         print output
+
+        def iso_function(self):
+            pass
+
+        def aperture_function(self):
+            pass
+
+        def inter_function(self):
+            pass
+
+        def live_function(self):
+            pass
+
+        def info_function(self):
+            pass
